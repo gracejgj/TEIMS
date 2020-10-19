0094ae2e38580340de047edeaf3b464deaa2a740
Jgrace <39512456+Gracej06@users.noreply.github.com>
2020-10-19 17:33:32 +0800

    Add files via upload

--- supplier/inc/session.php
+++ supplier/inc/session.php
@@ -1,1 +1,13 @@
-
+<?php
+	include 'dbconnect.php';
+	session_start();
+	if (!isset($_SESSION['supplier_login']))
+	{
+		?>
+        <script>
+			alert("You have not login yet");
+			window.location.href = "../index.php";
+		</script>
+        <?php
+	}
+?>
