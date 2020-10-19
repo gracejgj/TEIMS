83a9da923d005cbf839867f877092dfe5412b10c
Jgrace <39512456+Gracej06@users.noreply.github.com>
2020-10-19 17:41:13 +0800

    Add files via upload

--- clients/inc/session.php
+++ clients/inc/session.php
@@ -1,1 +1,13 @@
-
+<?php
+	include '../includes/dbconnect.php';
+	session_start();
+	if (!isset($_SESSION['client_login']))
+	{
+		?>
+        <script>
+			alert("You have not login yet");
+			window.location.href = "../index.php";
+		</script>
+        <?php
+	}
+?>
